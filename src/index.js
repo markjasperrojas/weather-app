c4bc680ed77336640a0d241cb0269bc63219e405
Mark Jasper Rojas <markjasperrojas1@gmail.com>
2026-08-31 01:55:53 +0800

    Implement form for location input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import { getWeatherData, getTemperature } from './weather.js';
+
+const form = document.querySelector('form');
+const locationInput = document.getElementById('location');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const location = locationInput.value;
+
+  const weatherData = await getWeatherData(location);
+  const temperature = getTemperature(weatherData);
+
+  console.log(temperature);
+
+  locationInput.value = '';
+});
